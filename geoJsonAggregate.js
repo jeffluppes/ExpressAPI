a08d53d16976eff6eed78a5bf374ca72f4baa8a9
Jeff Luppes <jeffluppes@gmail.com>
2015-05-07 15:12:20 +0200

    Fixed Geojson audit trail query. Hurrah for manual labour!

--- geoJsonAggregate.js
+++ geoJsonAggregate.js
@@ -8,16 +8,35 @@ db.planes4mei.aggregate([
     { '_id': '$properties.id',
       'type': { $first: "$type" },
       "gtype": { $first: "$geometry.type" },
-      'coordinates': { $push: '$geometry.coordinates'}
+      'coordinates': { $push: '$geometry.coordinates'},
+      'Altitude': {$push: '$properties.Altitude'},
+      'Track': {$push: '$properties.Track'},
+      'Speed': {$push: '$properties.Speed'},
+      'Squawk': {$first: '$properties.Squawk'},
+      'Time': {$push: '$properties.Time'},
+      'FeatureTypeId': {$first: '$properties.FeatureTypeID'},
+      'PlaneType': {$first: '$properties.planeType'},
+      'Active': {$push: "$properties.Active"}
     }
   },
   {$project:
     {
-      "id": 1,
+      "_id": 1,
       "type": 1,
       "geometry": {
           "type": "$gtype",
           "coordinates": "$coordinates"
+      },
+      "properties": {
+        "_id": "$_id",
+        "FeatureTypeId": "$FeatureTypeId",
+        "Altitude": "$Altitude",
+        "Track": "$Track",
+        "Speed": "$Speed",
+        "Squawk": "$Squawk",
+        "PlaneType": "$PlaneType",
+        "Active": "$Active",
+        "Time": "$Time"
       }
     }
   }
--- index.js
+++ index.js
@@ -139,21 +139,47 @@ app.get('/collections/:collectionName/track', function(req, res) {
 //TODO: Audittrail: returns a single document with $pushed items of everything.
 app.get('/collections/:collectionName/auditTrail/:id', function(req, res) {
   req.collection.aggregate([
-    {
-      "$match":
+      { $match:
         {
-          'properties.id': req.params.id
+          'properties.id': id(req.params.id)
         }
-    },
-    {
-      "$group":
+      },
+      {$group:
+        { '_id': '$properties.id',
+          'type': { $first: "$type" },
+          "gtype": { $first: "$geometry.type" },
+          'coordinates': { $push: '$geometry.coordinates'},
+          'Altitude': {$push: '$properties.Altitude'},
+          'Track': {$push: '$properties.Track'},
+          'Speed': {$push: '$properties.Speed'},
+          'Squawk': {$first: '$properties.Squawk'},
+          'Time': {$push: '$properties.Time'},
+          'FeatureTypeId': {$first: '$properties.FeatureTypeID'},
+          'PlaneType': {$first: '$properties.planeType'},
+          'Active': {$push: "$properties.Active"}
+        }
+      },
+      {$project:
         {
-          '_id': 'properties.id',
-          'geometry.coordinates':
-              { $push:
-                    { coordinates: '$geometry.coordinates'}}
+          "_id": 1,
+          "type": 1,
+          "geometry": {
+              "type": "$gtype",
+              "coordinates": "$coordinates"
+          },
+          "properties": {
+            "_id": "$_id",
+            "FeatureTypeId": "$FeatureTypeId",
+            "Altitude": "$Altitude",
+            "Track": "$Track",
+            "Speed": "$Speed",
+            "Squawk": "$Squawk",
+            "PlaneType": "$PlaneType",
+            "Active": "$Active",
+            "Time": "$Time"
+          }
         }
-    }
+      }
   ],
   function(e, results){
     if (e) return e;
